fc5a6809c8d530c89d703205b31ee3889306c046
jnwcki <jude.nowicki@gmail.com>
2016-03-01 13:08:32 -0500

    loaded working index.html

--- shorten_app/models.py
+++ shorten_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_version = models.CharField(max_length=30)
+    added = models.DateTimeField(auto_now_add=True)
+
+class Clicks(models.Model):
+    time_of_click = models.DateTimeField(auto_now_add=True)
+    referenced_url = models.ForeignKey(Url)
--- shorten_app/views.py
+++ shorten_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html', {})
+
+
+class UserClick(View):
+    pass
